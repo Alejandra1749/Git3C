3ca7f7f39e268a2f803205bf74871888b4eae637
Alejandra1749. <pamelaalejandra060328@gmail.com>
2025-07-17 10:56:47 -0600

    Con clase v4

---
+++ src/git3c/Clase.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package git3c;
+
+/**
+ *
+ * @author pamel
+ */
+public class Clase {
+    //iniciar
+}
